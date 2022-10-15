fc0fa260696725561427275633413ce32d219f52
endtoni <endtoni@gmail.com>
2022-10-14 22:58:21 -0700

    tested reference

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
-
 import os
-
 from pxr import Usd, UsdGeom
-import shutil
-
 
 def setup_workarea(root):
     out_dir = os.path.join(root, 'output')
     if not os.path.exists(out_dir):
         os.makedirs(out_dir)
+    os.chdir(out_dir)
     return out_dir
 
 
@@ -21,21 +18,32 @@ def get_stage(full_path):
 if __name__ == '__main__':
     directory = os.path.dirname(__file__)
     output_dir = setup_workarea(directory)
-
     main_stage_file = os.path.join(output_dir, 'main.usda')
     ref_stage_file = os.path.join(output_dir, 'ref.usda')
 
     main_stage = get_stage(main_stage_file)
-    ref_stage = get_stage(ref_stage_file)
+
 
     main_stage.RemovePrim('/root')
 
     rootTransformPrim = main_stage.DefinePrim('/root', 'Xform')
+    main_stage.SetDefaultPrim(rootTransformPrim)
+
     sphereTransformPrim = main_stage.DefinePrim('/root/sphere', 'Xform')
 
+    # Move sphere to (4, 5, 6)
     sphereTransformApi = UsdGeom.XformCommonAPI(sphereTransformPrim)
     sphereTransformApi.SetTranslate((4, 5, 6))
     cubeTransformPrim = main_stage.DefinePrim('/root/cube', 'Xform')
     sphereShapePrim = UsdGeom.Sphere.Define(main_stage, '/root/sphere/sphereShape')
     cubeShapePrim = main_stage.DefinePrim('/root/cube/cubeShape', 'Cube')
     main_stage.GetRootLayer().Save()
+
+
+    ref_stage = get_stage(ref_stage_file)
+    ref_sphere = ref_stage.OverridePrim('/refSphere')
+    references = ref_sphere.GetReferences()
+    references.ClearReferences()
+    references.AddReference(main_stage_file)
+    print(ref_stage.GetRootLayer().ExportToString())
+    ref_stage.GetRootLayer().Save()
--- usdview.bat
+++ usdview.bat
@@ -3,4 +3,4 @@ set PATH=C:\Program Files\Autodesk\Maya2022\bin;C:\Program Files\Autodesk\MayaUS
 set PYTHONPATH=C:\Program Files\Autodesk\MayaUSD\Maya2022\0.8.0\mayausd\USD3\lib\python;%PYTHONPATH%
 
 mayapy -m pip install PyOpenGL==3.1.0
-mayapy "C:\Program Files\Autodesk\MayaUSD\Maya2022\0.8.0\mayausd\USD3\bin\usdview" "C:\Users\Administrator\Documents\usdTest\output\main.usda"
+mayapy "C:\Program Files\Autodesk\MayaUSD\Maya2022\0.8.0\mayausd\USD3\bin\usdview" "C:\Users\Administrator\Documents\usdTest\output\ref.usda"
